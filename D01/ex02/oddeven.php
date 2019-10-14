c30052448a0ff28c28b0f6258966fa1c630cb7e1
Frédéric FOISSEY <frederic.foissey@gmail.com>
2019-10-14 19:38:26 +0200

    [D01] ex02

---
+++ D01/ex02/oddeven.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php
+<?php
+	while (1)
+	{
+		echo "Entrez un nombre: ";
+		$nb = rtrim(fgets(STDIN));
+		if (feof(STDIN) == true)
+			break;
+		if (is_numeric($nb) == true)
+		{
+			echo "Le chiffre $nb " ;
+			if ($nb % 2 == true)
+				echo "Impair\n"; 
+			else
+				echo "Pair\n"; 
+		}
+		else
+			echo "'$nb' n'est pas un chiffre\n"; 
+	}
+?>
