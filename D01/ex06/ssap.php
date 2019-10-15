81412391a417ae6b99c7e6f7fb7ae3460ac15aef
Frederic FOISSEY <frederic.foissey@gmail.com>
2019-10-15 11:29:50 +0200

    [D01] ex06: fix

--- D01/ex06/ssap.php
+++ D01/ex06/ssap.php
@@ -1,7 +1,15 @@
 #!/usr/bin/php
 <?php
-	$out = array_slice($argv, 1);
-	sort($out);
-	foreach($out as $elem)
-		echo $elem."\n";
+function epur_str($str)
+{
+	$out = preg_replace('/ +/', ' ', $str);
+	$out = preg_replace('/^ | $/', '', $out);
+	return ($str);
+}
+
+$str = implode(' ', array_slice($argv, 1));
+$out = explode(' ', epur_str($str));
+sort($out);
+foreach($out as $elem)
+	echo $elem."\n";
 ?>
