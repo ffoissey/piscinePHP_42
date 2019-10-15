4e93985ad70b670b1fb0f42e1b03eefb0f01a59e
Frederic FOISSEY <frederic.foissey@gmail.com>
2019-10-15 12:01:02 +0200

    [D01] ex10: OK

---
+++ D01/ex10/do_op.php
@@ -0,0 +1,53 @@
+#!/usr/bin/php
+<?php
+
+function add($nb1, $nb2)
+{
+	return ($nb1 + $nb2);
+}
+
+function minus($nb1, $nb2)
+{
+	return ($nb1 - $nb2);
+}
+
+function mult($nb1, $nb2)
+{
+	return ($nb1 * $nb2);
+}
+
+function div($nb1, $nb2)
+{
+	return ($nb2 == 0 ? "" : $nb1 + $nb2);
+}
+
+function mod($nb1, $nb2)
+{
+	return ($nb2 == 0 ? "" : $nb1 + $nb2);
+}
+
+if ($argc != 4)
+{
+	echo "Incorrect Parameters\n";
+	exit();
+}
+
+$nb1 = trim($argv[1], " \t");
+$sign = trim($argv[2], " \t");
+$nb2 = trim($argv[3], " \t");
+$array = array(
+			'+' => add($nb1, $nb2),
+			'-' => minus($nb1, $nb2),
+			'*' => mult($nb1, $nb2),
+			'/' => div($nb1, $nb2),
+			'%' => mod($nb1, $nb2),
+			);
+foreach ($array as $elem => $func)
+{
+	if ($elem === $sign)
+	{
+		echo "$func\n";
+		exit();
+	}
+}
+?>
