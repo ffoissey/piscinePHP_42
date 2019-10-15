75660b3e7132f7753ad612bfd99b4feb70f0a6a5
Frederic FOISSEY <frederic.foissey@gmail.com>
2019-10-15 10:50:12 +0200

    [D00] ex08: fix

--- D01/ex08/ft_is_sort.php
+++ D01/ex08/ft_is_sort.php
@@ -3,7 +3,8 @@
 function ft_is_sort($array)
 {
 	$sort_array = $array;
-	sort($sort_array);
+	if (empty($array) == false)
+		sort($sort_array);
 	while (empty($array) == false)
 	{
 		if (array_shift($array) !== array_shift($sort_array))
